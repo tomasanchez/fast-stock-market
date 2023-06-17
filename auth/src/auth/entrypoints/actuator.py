24783f72b0bdc4a1afa86ebfa1c9ce198b7805b9
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-06-17 10:46:37 -0300

    feat(entrypoints): readiness probe

--- auth/src/auth/entrypoints/actuator.py
+++ auth/src/auth/entrypoints/actuator.py
@@ -2,8 +2,11 @@
 Actuator Entry Point.
 """
 from fastapi import APIRouter, status, HTTPException
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.errors import ConnectionFailure
 from starlette.responses import RedirectResponse
 
+from auth.dependencies import ClientFactoryDependency
 from auth.domain.events import StatusChecked, ReadinessChecked
 from auth.domain.models import ServiceStatus
 from auth.domain.schemas import ResponseModel
@@ -14,13 +17,14 @@ router = APIRouter(tags=["Actuator"])
 @router.get("/readiness",
             status_code=status.HTTP_200_OK,
             summary="Readiness probe.")
-async def readiness() -> ResponseModel[ReadinessChecked]:
+async def readiness(mongo_factory: ClientFactoryDependency) -> ResponseModel[ReadinessChecked]:
     """
     Checks if the service is ready to accept requests.
     """
-    services_status: list[StatusChecked] = list()
+    mongo_status = await _ping_database(client=mongo_factory())
+    services_status: list[StatusChecked] = [mongo_status]
 
-    readiness_checked = ReadinessChecked(status=ServiceStatus.ONLINE)
+    readiness_checked = ReadinessChecked(status=ServiceStatus.ONLINE, services=services_status)
 
     if any([service for service in services_status if service.status == ServiceStatus.OFFLINE]):
         raise HTTPException(status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=readiness_checked.dict())
@@ -48,3 +52,28 @@ def root_redirect():
     Redirects the root path to the docs.
     """
     return RedirectResponse(url="/docs", status_code=status.HTTP_301_MOVED_PERMANENTLY)
+
+
+####################################################################################################
+# Internal Methods
+####################################################################################################
+
+async def _ping_database(client: AsyncIOMotorClient) -> StatusChecked:
+    """
+    Pings the database.
+
+    Args:
+        client (MongoClient): The database client.
+
+    Returns:
+        StatusChecked: The status of the database.
+    """
+    service_status = StatusChecked(name="mongodb", status=ServiceStatus.ONLINE)
+
+    try:
+        await client.admin.command("ping")
+    except ConnectionFailure as e:
+        service_status.status = ServiceStatus.OFFLINE
+        service_status.detail = str(e)
+
+    return service_status
